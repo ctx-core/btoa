f508b160e3d9f9dee4322e64ca62aa1d3fb72019
Brian Takita <brian.takita@gmail.com>
2021-05-24 19:40:39 -0400

    ## 5.2.0
    ### Minor Changes
    
    - dist,types directory: addressing typescript build issues
    
    ### Patch Changes
    
    - Updated dependencies [undefined]
    - @ctx-core/dom@8.4.0

--- src/_btoa_Uint32Array.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * btoa helper functions
- * @module @ctx-core/btoa/lib
- */
-export declare function _btoa_Uint32Array(b64: string): Uint32Array;
-export { _btoa_Uint32Array as _Uint32Array__btoa };
--- src/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './_btoa';
-export * from './_btoa_Uint32Array';
-export * from './btoa';
