c0f2bbbaa8b68444b25e12b673295fc238ce4e32
Brian Takita <brian.takita@gmail.com>
2021-05-25 01:29:12 -0400

    ## 5.3.0
    ### Minor Changes
    
    - fix tsc build directory issues
    
    ### Patch Changes
    
    - Updated dependencies [undefined]
    - @ctx-core/dom@8.5.0

--- src/_btoa.js
+++
@@ -1,7 +0,0 @@
-import { no_dom } from '@ctx-core/dom';
-export function _btoa() {
-    const btoa = no_dom
-        ? require('btoa-lite')
-        : window.btoa;
-    return btoa;
-}
--- src/btoa.js
+++
@@ -1,4 +0,0 @@
-import { _btoa } from './_btoa';
-export function btoa(data) {
-    return _btoa()(data);
-}
