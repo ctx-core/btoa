3876be7d7ad9c520e32449e0de0fa1d059d92ce0
Brian Takita <brian.takita@gmail.com>
2023-04-04 17:44:03 -0400

    formatting

--- src/btoa/index.d.ts
+++ src/btoa/index.d.ts
@@ -1,1 +1,1 @@
-export declare function btoa(decodedData: string): string
+export declare function btoa(decodedData:string):string
--- src/btoa_/index.browser.d.ts
+++ src/btoa_/index.browser.d.ts
@@ -1,2 +1,4 @@
-export declare function btoa_():((data:string)=>string)&typeof globalThis.btoa;
+export declare function btoa_():(
+	(data:string)=>string)
+	&typeof globalThis.btoa
 export { btoa_ as _btoa, }
